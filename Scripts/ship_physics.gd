7931a738445ca203e6bd758f0556f95a10dd9da4
ross <20172491+radix-r@users.noreply.github.com>
2025-08-29 18:36:36 -0400

    Fixed staggering from caused by mouse input during physics movement

--- Scripts/ship_physics.gd
+++ Scripts/ship_physics.gd
@@ -9,10 +9,7 @@ extends RigidBody3D
 #####################################
 # CONSTANTS
 #####################################
-# TODO: make data driven
-const spark_speed_coeficent: float = 0.3
-const spark_amount_ratio_coeficent: float = 0.1
-const spark_lifetime_coeficent: float = 0.1
+
 
 #####################################
 # EXPORT VARIABLES
@@ -28,6 +25,8 @@ const spark_lifetime_coeficent: float = 0.1
 #####################################
 # PRIVATE VARIABLES
 #####################################
+var tick_y_rotation_input_sum: float = 0
+var tick_x_rotation_input_sum: float = 0
 
 #####################################
 # ONREADY VARIABLES
@@ -38,9 +37,15 @@ const spark_lifetime_coeficent: float = 0.1
 @onready var camera_control: Node3D = $PitchPoint/CameraControl
 @onready var camera: Camera3D = $PitchPoint/CameraControl/Camera3D
 
-@onready var camera_chase: float = 0
+@onready var camera_chase: float =not 0
 @onready var contact_point: Vector3 = Vector3.ZERO
 
+@onready var spark_speed_coeficent: float 
+@onready var spark_amount_ratio_coeficent: float 
+@onready var spark_lifetime_coeficent: float 
+@onready var rotation_speed: float
+
+# TODO: Controler support
 func _input(event: InputEvent) -> void:
     #capture mouse movements
     if event is InputEventMouseButton:
@@ -50,33 +55,46 @@ func _input(event: InputEvent) -> void:
 
     if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
         if event is InputEventMouseMotion:
-            # self.rotate_y(-event.relative.x * rotation_speed)
-            # pitch_point.rotate_x(-event.relative.y * rotation_speed)
-            pitch_point.rotation.x = clamp(pitch_point.rotation.x, deg_to_rad(-85), deg_to_rad(85))
-
+            tick_y_rotation_input_sum += -event.relative.x 
+            tick_x_rotation_input_sum += -event.relative.y
+            
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
     if 0 < state.get_contact_count():
         contact_point = state.get_contact_collider_position(0)
         apply_spark_effect(contact_point)
 
+
 func _physics_process(delta: float) -> void:
     var input_dir: Vector3 = get_input_direction()
 
-    var thrust: Vector3 = calc_thrust(input_dir, delta)
+    self.rotate_y(tick_y_rotation_input_sum * rotation_speed * delta)
+    tick_y_rotation_input_sum = 0
+    pitch_point.rotate_x(tick_x_rotation_input_sum * rotation_speed * delta)
+    tick_x_rotation_input_sum = 0
+    pitch_point.rotation.x = clamp(pitch_point.rotation.x, deg_to_rad(-85), deg_to_rad(85))
+
+    var thrust: Vector3 = calc_thrust(input_dir, delta).rotated(Vector3.UP, global_rotation.y)
     
     apply_force(thrust)
-    if thrust.length() > 0:
-        print_debug(thrust)
+    #if thrust.length() > 0:
+        #print_debug(thrust)
     # TODO draw thrust vector
     
-    get_colliding_bodies()
-    pass
 
 func _ready() -> void:
     contact_monitor = true
     max_contacts_reported = 1
+    
+    var unit_name = "ShipPhysics"
+    
+    rotation_speed = Utilities.data_dict[unit_name]["rotation_speed"]
+    camera_chase = Utilities.data_dict[unit_name]["camera_chase"]
+    spark_speed_coeficent = Utilities.data_dict[unit_name]["spark_speed_coeficent"]
+    spark_amount_ratio_coeficent = Utilities.data_dict[unit_name]["spark_amount_ratio_coeficent"]
+    spark_lifetime_coeficent = Utilities.data_dict[unit_name]["spark_lifetime_coeficent"]
 
+# TODO: factor out of ship code
 func apply_spark_effect(global_location: Vector3) -> void:
     if linear_velocity.length() > 0.2:
         var new_sparks: GPUParticles3D = spark_effect.instantiate() as GPUParticles3D
@@ -96,7 +114,8 @@ func calc_thrust(input_dir: Vector3, delta: float) -> Vector3:
 func get_input_direction() -> Vector3:
     var input_right = Input.get_axis("left", "right")
     var input_up = Input.get_axis("down","up")
-    var input_forward = Input.get_axis( "back", "forward")
+    # -z is forward
+    var input_forward = Input.get_axis("forward", "back")
     return Vector3(input_right, input_up, input_forward)
 
 
